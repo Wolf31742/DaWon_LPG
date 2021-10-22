f58fa328e5fe1458f708de1bf78289888c1daf70
김동현 <xotjddl1114@gmail.com>
2021-10-22 19:15:18 +0900

    Initial commit

---
+++ src/main/java/com/example/dawon_lpg/DaWonLpgApplication.java
@@ -0,0 +1,13 @@
+package com.example.dawon_lpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaWonLpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaWonLpgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dawon_lpg/DaWonLpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dawon_lpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaWonLpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
